99cdc8f5882d0f1b9aaa6cd11fdc631b9e384c19
AHSAN RAZA <muhmmadahsan.seo@gmail.com>
2025-04-06 00:31:32 +0500

    new-tab-commit

--- src/components/translator/translator.js
+++ src/components/translator/translator.js
@@ -3,45 +3,52 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function Translator() {
-    let [text , setText] = useState("");
-    let [isArabic , setIsArabic ] = useState(false);
-    let [translatedState , setTranslatedState ]  = useState("")
- 
-    const toggleHandler = async ()=>{
+  let [text, setText] = useState("");
+  let [isArabic, setIsArabic] = useState(false);
+  let [translatedState, setTranslatedState] = useState("");
+  let [showTranslated, setShowTranslated] = useState(false);
 
-        const response = await axios.post('/api/translate/' , {
-            text : text ,
-            to : isArabic ? "en" : "ar"
-        })
-        console.log(response.data);
-        setTranslatedState(response.data.text)
-        
-        setIsArabic(!isArabic)
-    }
+  const toggleHandler = async () => {
+
+    const response = await axios.post('/api/translate/', {
+      text: text,
+      to: isArabic ? "en" : "ar"
+    })
+    console.log(response.data);
+    setTranslatedState(response.data.text)
+    setShowTranslated(!showTranslated);
+    setIsArabic(!isArabic)
+  }
   return (
     <div className='container mt-5'>
-      <div className='mb-3'>
-      <textarea 
-      className='form-control'
-       rows="4"
-      value={text}
-      onChange={(e)=> setText(e.target.value)} 
-
-      placeholder='Type your text here'/>
-
-</div>
-      <button className='btn btn-primary mb-3' onClick={toggleHandler }>Translate to {isArabic ? "English" : "Arabic"}</button>
-
       
-        {
-          translatedState &&( 
-            <div className="alert alert-success">
-          <h3>Translated State:</h3>
-          <p>{translatedState}</p>
-          </div>)
-        }
 
+        <div className='mb-4' >
+          <button className= {`btn me-3 ${!showTranslated ? 'btn-primary' :  'btn-outline-primary'}`} 
+          onClick={() => { setShowTranslated(false) }}>Original </button>
+          <button className= {`btn ${showTranslated ? 'btn-primary' :  'btn-outline-primary'}`} 
+          onClick={() => { setShowTranslated(true) }}>Translated</button>
+        </div>
+
+       { !showTranslated ? ( <textarea
+          className='form-control mb-3'
+          rows="4"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+
+          placeholder='Type your text here' />
+
+    ) :(
+      <div className="alert alert-success ">
+
+        <p >{translatedState}</p>
+      </div>)
+}
+
+      <button className='btn btn-primary mb-3' onClick={toggleHandler}>
+        Translate to {isArabic ? "English" : "Arabic"}</button>
     </div>
+
   )
 }
 
